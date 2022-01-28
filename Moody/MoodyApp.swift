1aecea49f98e37cea6a64563732c3d511b338867
valentinabgrando <valentinabgrando@gmail.com>
2022-01-27 19:11:54 -0500

    Initial Commit

---
+++ Moody/MoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp.swift
+//  Moody
+//
+//  Created by Valentina Grando on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
